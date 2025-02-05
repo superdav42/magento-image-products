d187e0d32fbee77880d23f152c38fd7b32a48650
David Stone <david@nnucomputerwhiz.com>
2025-02-05 10:06:20 -0700

    Add new attributes

---
+++ Setup/Patch/Data/AddFreshnessAttribute.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DevStone\ImageProducts\Setup\Patch\Data;
+
+use DevStone\ImageProducts\Model\Product\Type;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Setup\EavSetup;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddFreshnessAttribute implements DataPatchInterface
+{
+
+    protected ModuleDataSetupInterface $moduleDataSetup;
+    protected EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'freshness',
+            [
+                'type' => 'decimal',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Freshness',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => false,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'unique' => false,
+                'apply_to' => Type::TYPE_ID,
+                'used_in_product_listing' => false,
+                'is_html_allowed_on_front' => false,
+                'is_wysiwyg_enabled' => true,
+                'is_used_for_promo_rules' => true, // doesn't work. Need to update catalog_product_eav manually.
+            ]
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+        return $this;
+    }
+}
